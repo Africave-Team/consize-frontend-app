ccda8fc456e082a7ffe7aa255c172d51567c7d1f
Emmanuel Onoja <emmanuel@steadyinstall.com>
2024-05-17 17:09:51 +0100

    updated message for QR code

--- src/components/Dashboard/CourseQRCode.tsx
+++ src/components/Dashboard/CourseQRCode.tsx
@@ -3,7 +3,7 @@ import { useQRCode } from 'next-qrcode'
 
 export default function CourseQRCode ({ shortCode, courseName, teamName }: { shortCode: string, courseName: string, teamName: string }) {
   const { Canvas } = useQRCode()
-  const message = `Hello consize, \nI want to start the course *${courseName}* offered by *${teamName}*.\n_${shortCode}_`
+  const message = `Hello Consize, \nI want to start the course *${courseName}* offered by *${teamName}* (id: _${shortCode}_)`
   const url = `https://wa.me/${process.env.NEXT_PUBLIC_WHATSAPP_PHONENUMBER}?text=${encodeURIComponent(message)}`
   return (
     <Canvas
