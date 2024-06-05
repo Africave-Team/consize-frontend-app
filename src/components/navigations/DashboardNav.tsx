3fdda856625ee53d4aaca83184ba2bbd998d69f8
Emmanuel Onoja <emmanuel@steadyinstall.com>
2024-06-05 07:30:44 +0100

    fixed invite verification issue.

--- src/components/navigations/DashboardNav.tsx
+++ src/components/navigations/DashboardNav.tsx
@@ -3,16 +3,18 @@ import { useAuthStore } from '@/store/auth.store'
 import React from 'react'
 import Logo from '../Logo'
 import SubscriptionIndicator from '../SubscriptionIndicator'
+import { usePathname } from 'next/navigation'
 
 export default function DashboardNav () {
   const { team } = useAuthStore()
+  const pathname = usePathname()
   return (
     <div className='h-16 border-b flex justify-between items-center px-5'>
       <div className='flex items-center gap-3'>
         {team && team.logo ? <div>
           <img src={team.logo} className='h-10' />
         </div> : <Logo />}
-        <SubscriptionIndicator />
+        {!pathname.startsWith('/console') && <SubscriptionIndicator />}
       </div>
     </div>
   )
