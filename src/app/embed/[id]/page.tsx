6529053c094d7d3d5cc36560b66ebbf8e9cb1703
Emmanuel Onoja <emmanuel@steadyinstall.com>
2024-06-11 10:21:15 +0100

    reversed

--- src/app/embed/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default function Embeddable ({ params }: { params: { id: string } }) {
-  return <video className='h-screen w-screen' controls>
-    <source src={`https://storage.googleapis.com/kippa-cdn-public/microlearn-images/${params.id}.mp4`} />
-    Your browser does not support the video tag.
-  </video>
-}
