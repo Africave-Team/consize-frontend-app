351b87da556e735ef40be15d262ed1dc00a80afc
Emmanuel Onoja <emmanuel@steadyinstall.com>
2024-06-12 13:53:04 +0100

    added privacy policy page

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,106 @@
+import React from 'react'
+
+export default function page () {
+  return (
+    <div className='h-screen w-screen flex'>
+      <div className='md:w-2/6 w-0'></div>
+      <div className='flex-1 px-4 md:px-0 overflow-y-scroll'>
+        <div className='h-40 py-10'>
+          <div className='font-bold text-3xl'>Privacy policy</div>
+          <div className='w-72 flex items-center rounded-md px-3 h-11 mt-4 bg-gray-100'>
+            Last updated: 12th June, 2024
+          </div>
+        </div>
+        Thank you for using Consize (“we,” “us,” or “our”).
+        <div className='py-2'>
+          This Privacy Policy outlines the types of personal information we collect, how we use and protect that information, and your choices regarding your personal data.
+          Information We Collect:
+        </div>
+        <div className='font-medium'>
+          Personal Information:
+        </div>
+        <div className='py-2'>
+          We may collect personal information, including but not limited to names, phone numbers, and other contact details, to provide our services.
+        </div>
+        <div className='font-medium'>
+          Usage Information:
+        </div>
+        <div className="py-2">
+          We collect information about how users interact with our content, such as the time spent on pages and the content viewed.
+        </div>
+        <div className='font-semibold my-1'>
+          How We Use Your Information:
+        </div>
+        <div className='font-medium'>
+          Content Delivery:
+        </div>
+        <div className="py-2">
+          We use your personal information to deliver bite-sized content related to competitive government sector examinations.
+        </div>
+        <div className='font-medium'>
+          Communication:
+        </div>
+        <div className="py-2">
+          We may use your contact information to send updates, notifications, and relevant information about our services.
+        </div>
+        <div className='font-medium'>
+          Improvement of Services:
+        </div>
+        <div className="py-2">
+          We use collected data to analyze user behavior and improve the quality of our content and services.
+        </div>
+        <div className='font-semibold my-1'>
+          Information Sharing:
+        </div>
+        <div className='font-medium'>
+          Third-Party Services:
+        </div>
+        <div className="py-2">
+          We may use third-party services to facilitate our services, and they may have access to your personal information. However, they are obligated not to disclose or use the information for any other purpose.
+        </div>
+        <div className='font-medium'>
+          Legal Compliance:
+        </div>
+        <div className="py-2">
+          We may disclose personal information if required by law or in response to a valid legal request.
+        </div>
+        <div className='font-semibold my-1'>
+          Data Security:
+        </div>
+        <div className='font-medium'>
+          Security Measures:
+        </div>
+        <div className="py-2">
+          We implement industry-standard security measures to protect your personal information from unauthorized access, disclosure, alteration, and destruction.
+        </div>
+        <div className='font-semibold'>
+          Your Choices:
+        </div>
+        <div className='font-medium'>
+          Opt-Out:
+        </div>
+        <div className="py-2">
+          You can opt-out of receiving communications from us by following the instructions included in our messages.
+        </div>
+        <div className="font-semibold my-1">
+          Changes to This Privacy Policy:
+        </div>
+        <div className='font-medium'>
+          Updates:
+        </div>
+        <div className="py-2">
+          We may update this Privacy Policy periodically. We will notify you of any changes by posting the new Privacy Policy on this page.
+        </div>
+        <div className='font-medium'>
+          Contact Us:
+        </div>
+        <div className="py-2">
+          If you have any questions or concerns about this Privacy Policy, please contact us at <a className='text-purple-500' href="mailto:ketan@consize.com">ketan@consize.com</a>. By using our services, you agree to the terms outlined in this Privacy Policy.
+        </div>
+
+        <div className='h-40'></div>
+      </div>
+      <div className='md:w-36 w-0'></div>
+    </div>
+  )
+}
