a21e6292c154a9ef9806705ea2073e23dd1eb9db
Emmanuel Onoja <emmanuel@steadyinstall.com>
2024-07-12 14:53:28 +0100

    extract the subdomain

--- src/app/pageContent.tsx
+++ src/app/pageContent.tsx
@@ -4,7 +4,18 @@ import { useEffect } from 'react'
 
 export default function Home () {
   useEffect(() => {
-    // location.href = location.origin + '/auth/login'
+    let host = location.hostname
+    host = host.replace('app.', '').replace('staging-app.', '')
+    let parts = host.split('.')
+    parts.pop()
+    parts.pop()
+    if (parts.length > 0) {
+      let subdomain = parts[0]
+      console.log(subdomain)
+      // confirm the client from the subdomain
+    } else {
+      location.href = location.origin + '/courses'
+    }
   })
   return (
     <section className='h-screen overflow-y-scroll'>
