594196dc474ded7c84fdafebb6669f5d6ddffe93
Emmanuel Onoja <emmanuel@steadyinstall.com>
2024-07-12 19:53:19 +0100

    build error resolved

---
+++ src/app/teams/page.tsx
@@ -0,0 +1,10 @@
+import TeamNotFound from '@/components/TeamNotFound'
+import React from 'react'
+
+export default function page () {
+  return (
+    <div>
+      <TeamNotFound />
+    </div>
+  )
+}
