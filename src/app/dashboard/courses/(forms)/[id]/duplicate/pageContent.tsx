5e18cc143965f1192ec168387e069dde2153ba00
Emmanuel Onoja <emmanuel@steadyinstall.com>
2024-09-27 15:44:13 +0100

    add a debugger

--- src/app/dashboard/courses/(forms)/[id]/duplicate/pageContent.tsx
+++ src/app/dashboard/courses/(forms)/[id]/duplicate/pageContent.tsx
@@ -66,6 +66,7 @@ export default function DuplicateCoursePage ({ courseId }: { courseId: string })
         duration: 2000,
         isClosable: true,
       })
+      debugger
       let path = `/dashboard/courses/${data.id}/builder/outline`
       if (courseDetails?.data.bundle) {
         path = `/dashboard/courses/${data.id}/builder/publish`
