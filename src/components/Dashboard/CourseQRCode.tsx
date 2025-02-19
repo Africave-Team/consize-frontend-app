b54da40eea1ab5c277d98454a432714d535b11b6
Ahman Emmanuel <ahmanemmanuel@gmail.com>
2025-02-19 14:27:18 +0100

    updated welscome message

--- src/components/Dashboard/CourseQRCode.tsx
+++ src/components/Dashboard/CourseQRCode.tsx
@@ -3,7 +3,7 @@ import { useQRCode } from 'next-qrcode'
 
 export default function CourseQRCode ({ shortCode, courseName, teamName, cohort, phoneNumber }: { shortCode: string, courseName: string, teamName: string, cohort: string, phoneNumber: string }) {
   const { Canvas } = useQRCode()
-  const message = `Hello Consize, \nI want to start the course *${courseName}* offered by *${teamName}* \n(id: _${shortCode}_) \n(group: _${cohort}_)`
+  const message = `Hello, \nI want to start the course *${courseName}* offered by *${teamName}* \n(id: _${shortCode}_) \n(group: _${cohort}_)`
   const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`
 
   return (
